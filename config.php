06d7363f159e7498edc276bc02d374658cac561b
bnijboer <brendannijboer@gmail.com>
2020-09-22 10:46:48 +0200

    add screenshots for readme

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
       'database' => [
             'name'            => 'musicdb',
             'username'        => 'root',
-            'password'        => 'root',
+            'password'        => '',
             'connection'      => 'mysql:host=127.0.0.1',
             'options'         => [
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION     //throws exception error if error occurs. For example, if non-existent tablename is inserted in selectAll method.
